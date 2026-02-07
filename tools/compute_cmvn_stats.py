db6d79172c62a0dd881a3bab083adfc3331a047c
2026 Volume Perturbation <>
2026-02-07 12:52:25 +0100

    Patch CMVN stat computation to work with OPUS.

--- tools/compute_cmvn_stats.py
+++ tools/compute_cmvn_stats.py
@@ -30,19 +30,21 @@ class CollateFunc(object):
             value = item[1].strip().split(",")
             assert len(value) == 3 or len(value) == 1
             wav_path = value[0]
-            sample_rate = torchaudio.info(wav_path).sample_rate
-            resample_rate = sample_rate
-            # len(value) == 3 means segmented wav.scp,
-            # len(value) == 1 means original wav.scp
-            if len(value) == 3:
-                start_frame = int(float(value[1]) * sample_rate)
-                end_frame = int(float(value[2]) * sample_rate)
-                waveform, sample_rate = torchaudio.load(
-                    filepath=wav_path,
-                    num_frames=end_frame - start_frame,
-                    frame_offset=start_frame)
-            else:
-                waveform, sample_rate = torchaudio.load(item[1])
+            with open(wav_path, "rb") as f:
+                sample_rate = torchaudio.info(f).sample_rate
+                resample_rate = sample_rate
+                # len(value) == 3 means segmented wav.scp,
+                # len(value) == 1 means original wav.scp
+                if len(value) == 3:
+                    start_frame = int(float(value[1]) * sample_rate)
+                    end_frame = int(float(value[2]) * sample_rate)
+                    waveform, sample_rate = torchaudio.load(
+                        filepath=f,
+                        num_frames=end_frame - start_frame,
+                        frame_offset=start_frame)
+                else:
+                    with open(item[1], "rb") as f2:
+                        waveform, sample_rate = torchaudio.load(f2)
 
             waveform = waveform * (1 << 15)
             if self.resample_rate != 0 and self.resample_rate != sample_rate:
